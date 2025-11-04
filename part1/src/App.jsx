536b1cc2dbdd4add1cf16c4724186eb1bcfc5933
janbe30 <janbe30@gmail.com>
2025-11-04 15:30:11 -0600

    1.12 and 1.13

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -43,9 +43,38 @@ const App = () => {
     setPositivePct(positive.toFixed(1));
   };
 
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const min = 0;
+  const max = Math.floor(anecdotes.length);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(8).fill(0));
+
+  const handleAnecdotes = () => {
+    let target = Math.floor(Math.random() * (max - min) + min);
+    console.log(target);
+    setSelected(target);
+  };
+
+  const handleVotes = () => {
+    let copy = [...votes];
+    copy[selected] += 1;
+    setVotes([...copy]);
+  };
+
+  console.log(votes);
   return (
     <main>
-      <h1>Give feedback</h1>
+      <h2>Give feedback | Exercises 1.6 - 1.11 </h2>
       <div className="feedback-actions">
         <Button onClick={() => handleCount("good")} label="good" />
         <Button onClick={() => handleCount("neutral")} label="neutral" />
@@ -59,6 +88,19 @@ const App = () => {
         avg={avg}
         positivePct={positivePct}
       />
+
+      <hr />
+      <h2> Anecdotes | Exercise 1.12 </h2>
+      <div>
+        <p>
+          {anecdotes[selected]} <br />
+          <em>{votes[selected]} votes</em>
+        </p>
+      </div>
+      <div className="anecdotes-actions">
+        <button onClick={handleVotes}>Vote</button>
+        <button onClick={handleAnecdotes}>Show next</button>
+      </div>
     </main>
   );
 };
